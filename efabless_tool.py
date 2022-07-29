ab39a52239cc213164ce3c130e07d95d45ac64b5
Matt Venn <matt@mattvenn.net>
2022-07-29 11:22:31 +0200

    switch string formatting

--- efabless_tool.py
+++ efabless_tool.py
@@ -16,7 +16,26 @@ project_base_url = 'https://platform.efabless.com'
 cached_project_dir = 'cached_pages'
 
 # some projects don't have all keys, so set them to none
-minimum_project_keys = ['Last MPW Precheck', 'Last Tapeout', 'Git URL', 'MPW']
+key_map = { 
+    'Last MPW Precheck' : 'precheck',
+    'Last Tapeout'      : 'tapeout',
+    'Git URL'           : 'giturl',
+    'MPW'               : 'mpw',
+    'Owner'             : 'owner',
+    'Process'           : 'process',
+    'Summary'           : 'summary'
+    }
+
+format_map = {
+    'id'        : '{:5.5}',
+    'precheck'  : '{:10.10}',
+    'tapeout'   : '{:10.10}',
+    'giturl'    : '{:80.80}',
+    'mpw'       : '{:6.6}',
+    'owner'     : '{:20.20}',
+    'process'   : '{:8.8}',
+    'summary'   : '{:40.40}',
+    }
 
 # async code from https://gist.github.com/wfng92/2d2ae4385badd0f78612e447444c195f
 async def gather_with_concurrency(n, *tasks):
@@ -113,15 +132,17 @@ def parse_project_page():
             for div in divs:
                 key = div.h6.text
                 value = div.p.text.strip()
-                project[key] = value
+                if key in key_map:
+                    project[key_map[key]] = value
 
             mpw_header = soup.find("h1", {"class": "card-label h1 font-weight-bold pt-3 text-center"})
             if mpw_header is not None:
-                project['MPW'] = mpw_header.text.strip()
+                project['mpw'] = mpw_header.text.strip()
 
-            for key in minimum_project_keys:
+            # fill in any blanks
+            for key in key_map.values():
                 if not key in project:
-                    project[key] = None
+                    project[key] = 'n/a'
 
             projects.append(project)
 
@@ -135,18 +156,25 @@ def show_project(projects, id):
             for key in project:
                 logging.info("%-20s = %s" % (key, project[key]))
 
-def list_projects(projects):
+def list_projects(projects, fields):
     for project in projects:
-        logging.info("%-5s %-5s %-40s %-10s %-10s" % (project["id"], project["MPW"], project["Owner"], project["Last MPW Precheck"], project["Last Tapeout"]))
+        log = ''
+        # always include id
+        if 'id' not in fields:
+            fields = 'id,' + fields
+
+        for field in fields.split(','):
+            if field in project:
+                log += format_map[field].format(project[field])
+                log += " "
+
+        logging.info(log)
 
 def get_pins_in_lef(projects):
     from get_pins import get_pins
     max_pins = 0
     max_id = None
     for project in projects:
-        if not project["Last Tapeout"] == "Succeeded":
-            continue
-   
         pins = get_pins(project)
         if pins > max_pins:
             max_pins = pins
@@ -158,6 +186,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Efabless project tool")
 
     parser.add_argument('--list', help="list basic project info", action='store_const', const=True)
+    parser.add_argument('--fields', help="comma separated list of fields to show. To see all available fields, use the --show option", default='id,mpw,owner,precheck,tapeout')
     parser.add_argument('--show', help="show all data for a specific project")
     parser.add_argument('--get-pins', help="dump number of pins found in user project wrapper lef file", action='store_const', const=True)
     parser.add_argument('--update-cache', help='fetch the project data', action='store_const', const=True)
@@ -205,7 +234,7 @@ if __name__ == '__main__':
                         projects.append(project)
 
     if args.list:
-        list_projects(projects)
+        list_projects(projects, args.fields)
 
     if args.show:
         show_project(projects, args.show)
